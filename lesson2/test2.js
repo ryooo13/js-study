89b26875b4c5a1cf58d87a01d94149558a7a369a
ryo <>
2020-02-08 03:04:15 +0900

    lesson2_second

--- lesson2/test2.js
+++ lesson2/test2.js
@@ -1,3 +1,17 @@
 let messages = 'hello';
 
-alert(messages);
+// alert(messages);
+
+let name = 'john';
+let admin;
+
+admin = name;
+
+alert(admin);
+
+let = wePlanet;
+
+const birthday = '18.04.1982';
+
+const age = someCoder(birthday);
+
